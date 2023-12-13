39b91ae7657eb3899f3603e59f574339c5a1fe89
jieun615 <je060105@gmail.com>
2023-12-13 16:09:35 +0900

    프로젝트 세팅

---
+++ src/config/passport.js
@@ -0,0 +1,93 @@
+const passport = require('passport');
+const User = require('../models/users.model');
+const { access } = require('fs');
+const LocalStrategy = require('passport-local').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const KakaoStrategy = require('passport-kakao').Strategy;
+require('dotenv').config();
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+})
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+    .then(user => {
+        done(null, user);
+    })
+})
+
+const localStrategyConfig =  new LocalStrategy({ usernameField: 'email', passwordField: 'password'}, (email, password, done) => {
+    User.findOne({ email: email.toLocaleLowerCase() })
+        .then(user => {
+            if(!user) {
+                return done(null, false, { msg: `Email ${email} not found`});
+            }
+            user.comparePassword(password, (err, isMatch) => {
+                if(err) return done(err);
+
+                if(isMatch) {
+                    return done(null, user);
+                }
+                    return done(null, false, {msg : 'Invalid email or password.'})
+            });
+        })
+        .catch((err)=>{
+            return res.status(400).send(err);
+        }) 
+    }
+)
+
+passport.use('local', localStrategyConfig);
+
+const googleClientID = process.env.GOOGLE_CLIENT_ID
+const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET
+const googleStrategyConfig = new GoogleStrategy({
+    clientID: googleClientID,
+    clientSecret: googleClientSecret,
+    callbackURL: '/auth/google/callback',
+    scope: ['email', 'profile']
+}, async (accessToken, refreshToken, profile, done) => {
+    const existingUser = await User.findOne({googleId: profile.id})
+        if(existingUser) {
+            return done(null, existingUser);
+        } else {
+            const user = new User();
+            user.email = profile.emails[0].value;
+            user.googleId = profile.id;
+            try{
+                await user.save();
+            } catch(err){
+                console.log(err);
+                return done(err);
+            }
+            done(null, user);
+        }
+    }
+)
+
+passport.use('google', googleStrategyConfig);
+
+const kakaoStrategyConfig = new KakaoStrategy({
+    clientID: process.env.KAKAO_CLIENT_ID,
+    callbackURL: '/auth/kakao/callback',
+}, async (accessToken, refreshToken, profile, done) => {
+    const existingUser = await User.findOne({ kakaoId: profile.id })
+        if(existingUser){
+            return done(null, existingUser);
+        } else {
+            const user = new User();
+            user.kakaoId = profile.id;
+            user.email = profile._json.kakao_account.email;
+            try{
+                await user.save();
+            } catch {
+                console.log(err);
+                return done(err);
+            }
+            done(null, user);
+        }
+    }
+)
+
+passport.use('kakao', kakaoStrategyConfig);
---
+++ src/server.js
@@ -0,0 +1,68 @@
+const cookieSession = require('cookie-session');
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const path = require('path');
+const passport = require('passport');
+const { checkAuthenticated, checkNotAuthenticated } = require('./middlewares/auth');
+const cookieEncryptionKey = ['key1', 'key2'];
+const mainRouter = require('./routers/main.router');
+const usersRouter = require('./routers/users.router');
+const postsRouter = require('./routers/posts.router');
+const port = process.env.SERVER_PORT;
+
+app.use(cookieSession({
+    name: 'cookie-session-name',
+    keys: cookieEncryptionKey
+}))
+
+// register regenerate & save after the cookieSession middleware initialization
+app.use(function(request, response, next) {
+    if (request.session && !request.session.regenerate) {
+        request.session.regenerate = (cb) => {
+            cb()
+        }
+    }
+    if (request.session && !request.session.save) {
+        request.session.save = (cb) => {
+            cb()
+        }
+    }
+    next()
+})
+
+app.use(passport.initialize());
+app.use(passport.session());
+require('./config/passport');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log('mongodb connected')
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+app.use("/static", express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res, next) => {
+    setImmediate(() => { next( new Error('it is an error')); })
+})
+
+app.use((error, req, res, next) => {
+    res.json({ message: error.message });
+})
+
+app.use('/', mainRouter);
+app.use('/auth', usersRouter);
+app.use('/posts', postsRouter);
+
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
+});
