0411b13a54bc9e1704aa94a7ce679aa9b9fb647e
jieun615 <je060105@gmail.com>
2023-12-14 18:55:12 +0900

    에러 해결

--- src/routers/posts.router.js
+++ src/routers/posts.router.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const { checkAuthenticated } = require('../middlewares/auth');
+const Post = require('../models/posts.model');
+const Comment = require('../models/comments.model');
 
 router.get('/', checkAuthenticated, (req, res) => {
-    res.render('posts');
-})
+    Post.find()
+        .populate('comments')
+        .sort({ createdAt: -1 })
+        .then((posts) => {
+            res.render('posts', {
+                    posts: posts,
+                    currentUser: req.user
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
 
 module.exports = router;
