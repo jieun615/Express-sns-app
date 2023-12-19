a51ecad7b87cea6eb4065b9fa93083b444398aa9
jieun615 <je060105@gmail.com>
2023-12-19 14:08:54 +0900

    댓글 기능 구현

--- src/routers/comments.router.js
+++ src/routers/comments.router.js
@@ -1,4 +1,33 @@
 const express = require('express');
-const router = express.Router();
+const { checkAuthenticated } = require('../middlewares/auth');
+const router = express.Router({
+    mergeParams: true
+});
+const Post = require('../models/posts.model');
+const Comment = require('../models/comments.model');
+
+router.post('/', checkAuthenticated, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        const comment = await Comment.create(req.body)
+            try {
+                comment.author.id = req.user._id;
+                comment.author.username = req.user.username;
+                comment.save();
+
+                post.comments.push(comment);
+                post.save();
+                res.redirect('back');
+            } catch (err) {
+                res.redirect('back');
+            }
+    } catch (err) {
+        res.redirect('back');
+    }
+
+
+
+    
+})
 
 module.exports = router;
