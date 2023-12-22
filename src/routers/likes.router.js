0d6ddb6d7e68908b18db3e50dee9f21280932860
jieun615 <je060105@gmail.com>
2023-12-22 13:46:23 +0900

    좋아요기능 수정

--- src/routers/likes.router.js
+++ src/routers/likes.router.js
@@ -10,16 +10,13 @@ router.put('/posts/:id/like', checkAuthenticated, async (req, res) => {
             const updatedLikes = post.likes.filter(like => like !== req.user._id.toString());
             await Post.findByIdAndUpdate(post._id, {
                 likes: updatedLikes
-            }, (err, _) => {
-                res.redirect('back');
             })
         } else {
             await Post.findByIdAndUpdate(post._id, {
                 likes: post.likes.concat([req.user._id])
-            }, (err, _) => {
-                res.redirect('back');
             })
-        }
+        } 
+        res.redirect('back');
     } catch (err) {
         res.redirect('back');
     }
