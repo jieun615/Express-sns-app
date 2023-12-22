6bfbd65fff396ca644d06a5249c9d1670ab18094
jieun615 <je060105@gmail.com>
2023-12-22 13:39:36 +0900

    좋아요기능 구현

--- src/routers/likes.router.js
+++ src/routers/likes.router.js
@@ -1,4 +1,28 @@
 const express = require('express');
+const { checkAuthenticated } = require('../middlewares/auth');
 const router = express.Router();
+const Post = require('../models/posts.model');
+
+router.put('/posts/:id/like', checkAuthenticated, async (req, res) => {
+    const post = await Post.findById(req.params.id)
+    try {
+        if(post.likes.find(like => like === req.user._id.toString())) {
+            const updatedLikes = post.likes.filter(like => like !== req.user._id.toString());
+            await Post.findByIdAndUpdate(post._id, {
+                likes: updatedLikes
+            }, (err, _) => {
+                res.redirect('back');
+            })
+        } else {
+            await Post.findByIdAndUpdate(post._id, {
+                likes: post.likes.concat([req.user._id])
+            }, (err, _) => {
+                res.redirect('back');
+            })
+        }
+    } catch (err) {
+        res.redirect('back');
+    }
+});
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -90,7 +90,7 @@ app.use('/posts', postsRouter);
 app.use('/posts/:id/comments', commentsRouter);
 app.use('/profile/:id', profileRouter);
 app.use('/friends', friendsRouter);
-app.use('/posts/:id/like', likesRouter);
+app.use(likesRouter);
 
 app.use((err, req, res, next) => {
     res.status(err.status || 500);
