10e0a67e2bde206d96099c8bdb6366fa2e3adfc4
jieun615 <je060105@gmail.com>
2023-12-26 10:49:57 +0900

    friends 페이지를 위한 핸들러 생성

--- src/routers/friends.router.js
+++ src/routers/friends.router.js
@@ -1,4 +1,17 @@
 const express = require('express');
+const User = require('../models/users.model');
+const { checkAuthenticated } = require('../middlewares/auth');
 const router = express.Router();
+
+router.get('/', checkAuthenticated, async (req, res) => {
+    const users = await User.find({})
+        try {
+            res.render('friends', {
+                users : users
+            })
+        } catch (err) {
+            res.redirect('/posts');
+        }
+});
 
 module.exports = router;
