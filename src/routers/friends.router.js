dc6855691d18f40d956a8188f40c7c24fa999d4c
jieun615 <je060105@gmail.com>
2024-02-13 15:56:27 +0900

    friend 페이지 기능 생성

--- src/routers/friends.router.js
+++ src/routers/friends.router.js
@@ -14,4 +14,33 @@ router.get('/', checkAuthenticated, async (req, res) => {
         };
 });
 
+router.put('/:id/add-friend', checkAuthenticated, async (req, res) => {
+    const user = await User.findById(req.params.id, (err, user) => {
+        try {
+            User.findByIdAndUpdate(user._id, {
+                friendsRequests: user.friendsRequests.concat([req.user.id])
+            }, (err, _) => {
+                res.redirect('back');
+            })
+        } catch (err) {
+            res.redirect('back');
+        }
+    })
+})
+
+router.put('/:firstId/remove-friend-request/:secondId', checkAuthenticated, async (req, res) => {
+    const user = await User.findById(req.params.firstId, (err, user) => {
+        try {
+            const filteredFriendsRequests = user.friendsRequests.filter(friendId => friendId !== req.params.secondId);
+            User.findByIdAndUpdate(user._id, {
+                friendsRequests: filteredFriendsRequests
+            }, (err, _) => {
+                res.redirect('back');
+            })
+        } catch (err) {
+            res.redirect('back');
+        }
+    })
+})
+
 module.exports = router;
