32295448c33e8baa64605fa0a4e71aeceb519291
jieun615 <je060105@gmail.com>
2024-02-26 01:34:23 +0900

    Profile 페이지 데이터 생성

--- src/routers/profile.router.js
+++ src/routers/profile.router.js
@@ -1,4 +1,28 @@
 const express = require('express');
-const router = express.Router();
+const { checkAuthenticated } = require('../middlewares/auth');
+const router = express.Router({
+    mergeParams: true
+});
+const Post = require('../models/posts.model');
+const User = require('../models/users.model');
+
+router.get('/', checkAuthenticated, async (req, res) => {
+    try {
+        const posts = await Post.find({ "author.id": req.params.id })
+            .populate('comments')
+            .sort({ createdAt: -1 })
+            .exec();
+        const user = await User.findById(req.params.id);
+        if(!user) {
+            res.redirect('/back');
+        }
+        res.render('profile', {
+            posts: posts,
+            user: user
+        });
+    } catch (err) {
+        res.redirect('back');
+    }
+})
 
 module.exports = router;
